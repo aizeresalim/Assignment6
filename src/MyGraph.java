65ac4ac4fe686c67431257715d21763dbadf4eaa
aizeresalim <0424aizeka@gmail.com>
2023-05-31 22:41:37 +0600

    dijkstra method

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,15 +1,43 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class MyGraph<Vertex> {
-    private Map<Vertex, List<Edge<Vertex>>> adjList;
+    private Map<Vertex, List<Edge<Vertex>>> graph;
 
     public MyGraph() {
-        adjList = new HashMap<>();
+        if (!graph.containsKey(source)) graph.put(source, new ArrayList<>());
+        graph.get(source).add(edge);
     }
+    public Map<Vertex, Double> dijkstra(Vertex start) {
 
+        Map<Vertex, Double> distances = new HashMap<>();
+        for (Vertex node : graph.keySet()) {
+            distances.put(node, Double.MAX_VALUE);
+        }
+        distances.put(start, 0d);
+
+        PriorityQueue<Vertex> queue = new PriorityQueue<>(Comparator.comparingDouble(distances::get));
+        queue.add(start);
+
+
+        while (!queue.isEmpty()) {
+            Vertex currentVertex = queue.poll();
+
+            for (Edge neighbor : graph.get(currentVertex)) {
+
+                double distance = distances.get(currentVertex) + neighbor.getWeight();
+
+
+                if (distance < distances.get(currentVertex)) {
+                    distances.put(currentVertex, distance);
+
+                    queue.add((Vertex) neighbor.getDest());
+                }
+            }
+        }
+
+        return distances;
+    }
     public void addEdge(Vertex source, Vertex destination, double weight){
         Edge edge = new Edge(source, destination, weight);
         adjList.get(source).add(edge);
